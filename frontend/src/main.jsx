d2ca35b43e578c0f248e09efb5b6d41fe544caeb
sabrynavn <Sabrynanoronhav@gmail.com>
2026-05-27 17:58:35 -0300

    feat: configurar React Router com navegação entre páginas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/* Renderiza a aplicação React dentro da div com id="root" */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
